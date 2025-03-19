35eb391ffdf72aff60d78c49bf7f1aed55146d0a
jacob <jacob.opondo@student.moringaschool.com>
2025-03-19 15:17:34 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const checkbox = document.querySelector("#id-checkbox");
+
+checkbox.addEventListener("click", checkboxClick, false);
+
+function checkboxClick(event) {
+  const warn = "preventDefault() won't let you check this!\n";
+  document.getElementById("output-box").innerText += warn;
+  event.preventDefault();
+}
